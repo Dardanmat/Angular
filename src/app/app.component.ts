bf09bd70881bd544b7a3b069115d7b9447396c26
lai2-01 <lai2-01@itismeucci.com>
2022-06-01 13:40:27 +0200

    da finire

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', /*selector è intesa la tag app-root*/
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'learn-angular';
+  alunni : string[] = ["Bartoli", "Belfanti", "Bencini", "Berisha","Coppi"];
+
+  soggetto : any | null = null;
+
+  modifica(e:any){
+    this.soggetto = e;
+  }
+
 }
